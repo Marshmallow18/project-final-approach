22ddb02872f3a2a4c125cf91a342b740742b93aa
Victor Leao <vlab22@gmail.com>
2020-05-07 12:42:13 +0200

    Commit after first merge. Some unnecessary dir deleted and added to .gitignore

--- GXPEngine/GXPEngine/MyGame.cs
+++ GXPEngine/GXPEngine/MyGame.cs
@@ -33,29 +33,20 @@ public class MyGame : Game
 
     private FollowCamera _cam;
     private FpsCounter _fpsCounter;
+
+    private Player _player;
+    
+    /// <summary>
+    /// Debug GameObjects
+    /// </summary>
     private TextBox _debugText;
     private CircleGameObject _circleGo;
-
     private EasyDraw _debugColl;
     private EasyDraw _debugPOI;
     private Arrow _debugNormalPOI;
     
     public MyGame() : base(SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN) // Create a window that's 800x600 and NOT fullscreen
     {
-        //----------------------------------------------------example-code----------------------------
-        //create a canvas
-        Canvas canvas = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-        //add some content
-        canvas.graphics.FillRectangle(new SolidBrush(Color.Red), new Rectangle(0, 0, 400, 300));
-        canvas.graphics.FillRectangle(new SolidBrush(Color.Blue), new Rectangle(400, 0, 400, 300));
-        canvas.graphics.FillRectangle(new SolidBrush(Color.Yellow), new Rectangle(0, 300, 400, 300));
-        canvas.graphics.FillRectangle(new SolidBrush(Color.Gray), new Rectangle(400, 300, 400, 300));
-
-        //add canvas to display list
-        AddChild(canvas);
-        //------------------------------------------------end-of-example-code-------------------------
-
         string[] tmxFiles = TmxFilesLoader.GetTmxFileNames("Level*.tmx");
         var mapData = TiledMapParserExtended.MapParser.ReadMap(tmxFiles[0]);
 
@@ -66,15 +57,23 @@ public class MyGame : Game
         _cam = new FollowCamera(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
         AddChild(_cam);
 
+        _player = new Player();
+        AddChild(_player);
+        
+        _player.SetXY(1192, 4664);
+        
+        _cam.Target = _player;
+        _cam.Map = _caveLevelMap;
+        _cam.SetXY(1000, 1000);
+        
+        /*
+         * DEBUG
+         */
         _circleGo = new CircleGameObject(64, 64, (uint) Color.Red.ToArgb());
         var circleGo2 = new CircleGameObject(4, 4);
         AddChild(_circleGo);
         _circleGo.SetXY(1192, 4664);
         _circleGo.AddChild(circleGo2);
-
-        _cam.Target = _circleGo;
-        _cam.Map = _caveLevelMap;
-        _cam.SetXY(1000, 1000);
 
         _fpsCounter = new FpsCounter();
         AddChild(_fpsCounter);
@@ -96,68 +95,9 @@ public class MyGame : Game
         var mousePos = new Vector2Int(Input.mouseX, Input.mouseY);
         var worldMousePos = _cam.TransformPoint(mousePos.x, mousePos.y) - Vector2.right * (width / 2) * _cam.scaleX -
                             Vector2.up * (height / 2) * _cam.scaleY;
-        
-        Vector2 lastPos = _circleGo.Position;
-        
-        float camSpeed = 50;
-        Vector2 vel;
 
-        if (Input.GetKey(Key.W))
-        {
-            _circleGo.Translate(0, -camSpeed * Time.delta);
-            
-        }
-        else if (Input.GetKey(Key.S))
-        {
-            _circleGo.Translate(0, camSpeed * Time.delta);
-        }
-
-        if (Input.GetKey(Key.D))
-        {
-            _circleGo.Translate(camSpeed * Time.delta, 0);
-        }
-        else if (Input.GetKey(Key.A))
-        {
-            _circleGo.Translate(-camSpeed * Time.delta, 0);
-        }
-
-        var isWalkable = _caveLevelMap.IsWalkablePosition(_circleGo.Position);
-
-        if (!isWalkable)
-        {
-            var nextPos = _caveLevelMap.GetCollisionPOI(_circleGo.Position, lastPos);
-
-            var normalCollision = _caveLevelMap.GetCollisionNormal(nextPos);
-            
-            Console.WriteLine($"nextpos: {nextPos} | normal: {normalCollision}");
-            
-            _debugPOI?.SetXY(nextPos.x,nextPos.y);
-            _debugColl?.SetXY(_circleGo.x, _circleGo.y);
-
-            if (_debugNormalPOI != null)
-            {
-                _debugNormalPOI.startPoint = nextPos - _cam.Position + new Vector2(HALF_SCREEN_WIDTH,HALF_SCREEN_HEIGHT);
-                _debugNormalPOI.vector = normalCollision;
-            }
-
-            _circleGo.SetXY(lastPos.x, lastPos.y);
-        }
-        
-        if (Input.GetKey(Key.E))
-        {
-            _cam.scale += 0.05f;
-
-            for (int i = 0; i < _cam.GetChildren().Count; i++)
-            {
-            }
-        }
-        else if (Input.GetKey(Key.C))
-        {
-            _cam.scale -= 0.05f;
-        }
-
-        _debugText.TextValue =
-            $"camScale: {_cam.scale:0.00} | mousePos: {mousePos} | worldMousePos: {worldMousePos} | isWalk: {isWalkable}";
+        _debugText.TextValue = "";
+            //$"camScale: {_cam.scale:0.00} | mousePos: {mousePos} | worldMousePos: {worldMousePos} | isWalk: {isWalkable}";
 
         if (Input.GetKeyDown(Key.U))
         {
@@ -168,6 +108,8 @@ public class MyGame : Game
     private void ToogleDebug(Vector2 worldMousePos)
     {
         Debug = !Debug;
+
+        int playerIndex = _player.Index;
         
         //Draw walkable image layers
         var debugWalkable = this.GetChildren().FirstOrDefault(g => g.name == "Debug_Walkable");
@@ -181,9 +123,7 @@ public class MyGame : Game
                     name = "Debug_Walkable"
                 };
 
-                int circleIndex = _circleGo.Index;
-                
-                AddChildAt(debugWalkable, circleIndex);
+                AddChildAt(debugWalkable, playerIndex);
                 debugWalkable.SetXY(bitmapData.offSetX, bitmapData.offSetY);
             }
         }
